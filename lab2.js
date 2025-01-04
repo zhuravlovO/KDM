c33c46ab4fb59b20ee0968bbc7a094a5196bcb1a
zhuravlovO <zhuravlov.ov@gmail.com>
2025-01-04 12:28:01 +0200

    Update and rename lab2 to lab2.js

--- lab2.js
+++ lab2.js
@@ -1,14 +1,11 @@
-function asyncMapPromise(array, callback) {
+function asyncMapPromise(array, asyncOperation) {
   return Promise.all(
     array.map((item) => {
       if (item <= 0) {
         return Promise.reject(`Error: Array element ${item} must be greater than 0`);
       }
-      return new Promise((resolve, reject) => {
-        callback(item, (error, value) => {
-          if (error) reject(error);
-          else resolve(value);
-        });
+      return new Promise((resolve) => {
+         asyncOperation(item).then(resolve);
       });
     })
   );
@@ -17,36 +14,35 @@ function asyncMapPromise(array, callback) {
 // використання
 asyncMapPromise(
   [1, 2, 3],
-  (item, cb) => setTimeout(() => cb(null, item * 2), 100)
+  (item) => new Promise(resolve => setTimeout(() => resolve(item * 2), 100))
 )
-  .then((result) => console.log('Promise Results:', result)) 
+  .then((result) => console.log('Promise Results:', result))
   .catch((err) => console.error('Promise Error:', err));
 
 asyncMapPromise(
   [1, -2, 3],
-  (item, cb) => setTimeout(() => cb(null, item * 2), 100)
+   (item) => new Promise((resolve, reject) => setTimeout(() => resolve(item * 2), 100))
 )
   .then((result) => console.log('Promise Results:', result))
-  .catch((err) => console.error('Promise Error:', err)); 
+  .catch((err) => console.error('Promise Error:', err));
 
-
-//приклади await
+// приклади await
 async function demoAwait() {
   try {
     const result = await asyncMapPromise(
       [1, 2, 3],
-      (item, cb) => setTimeout(() => cb(null, item * 2), 100)
+      (item) => new Promise(resolve => setTimeout(() => resolve(item * 2), 100))
     );
     console.log('Await Results:', result); // [2, 4, 6]
   } catch (err) {
     console.error('Await Error:', err);
   }
 
-//з помилкою
+  //з помилкою
   try {
     const result = await asyncMapPromise(
       [1, -2, 3],
-      (item, cb) => setTimeout(() => cb(null, item * 2), 100)
+        (item) => new Promise(resolve => setTimeout(() => resolve(item * 2), 100))
     );
     console.log('Await Results:', result);
   } catch (err) {
